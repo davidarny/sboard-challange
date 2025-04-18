ae624124317e857843f4d3df72dd12d0371b1533
David Arutyunyan <arutiunian666@gmail.com>
2025-04-19 02:26:02 +0300

    fix(server): add global prefix 'api' to nest app

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -8,20 +8,22 @@ import { join } from "node:path";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api");
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
       package: "image",
       protoPath: join(__dirname, "image/image.proto"),
-      url: `0.0.0.0:${env.PORT}`,
+      url: `0.0.0.0:${env.PORT + 1}`,
     },
   });
 
   await app.startAllMicroservices();
-  Logger.log(`gRPC microservice is running on port ${env.PORT}`, "NestApplication");
+  Logger.log(`gRPC microservice is running on port ${env.PORT + 1}`, "NestApplication");
 
   await app.listen(env.PORT);
+  Logger.log(`HTTP server is running on port ${env.PORT}`, "NestApplication");
 }
 
 bootstrap().catch(console.error);
